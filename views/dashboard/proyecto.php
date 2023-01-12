e50c3f69615f88db9208a14c8f1c9eaac1cdfd50
Edgar Perez <edgarjoel9912@gmail.com>
2023-01-12 18:53:44 -0400

    ♻️ Refactoring

--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -5,6 +5,26 @@
         <button type="button" class="agregar-tarea" id="agregar-tarea">&plus; Nueva Tarea</button>
     </div>
 
+    <div id="filtros" class="filtros">
+        <div class="filtros-inputs">
+            <h2>Filtros:</h2>
+            <div class="campo">
+                <label for="todas">Todas</label>
+                <input type="radio" id="todas" name="filtro" value="" checked>
+            </div>
+
+            <div class="campo">
+                <label for="completadas">Completadas</label>
+                <input type="radio" id="completadas" name="filtro" value="1">
+            </div>
+
+            <div class="campo">
+                <label for="pendientes">Pendientes</label>
+                <input type="radio" id="pendientes" name="filtro" value="0">
+            </div>
+        </div>
+    </div>
+
     <ul id="listado-tareas" class="listado-tareas"></ul>
 </div>
 
@@ -13,7 +33,7 @@
 
 <?php
 
-$script = '
+$script .= '
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="build/js/tareas.js"></script>
 ';
